89e2493c938bf0e2aa9d8ae6af3f69740e098ae6
naka <9111754+ynakayu@users.noreply.github.com>
2021-08-24 11:03:32 +0900

    04-11_スタイルを設定しよう

---
+++ 04-11_add-style/js/main.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+  data: () => ({
+    newItem: '',
+    todos: []
+  }),
+  methods: {
+    addItem: function(event) {
+      // console.log('Clicked!')
+      if(this.newItem === '') return
+      let todo = {
+        item: this.newItem,
+        isDone: false
+      }
+      this.todos.push(todo)
+      this.newItem = ''
+    }
+  }
+})
+app.mount('#app')
