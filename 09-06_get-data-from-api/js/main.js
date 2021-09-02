474f63cbf1a6b57370dc043fc1d836f9dcb07a8e
naka <9111754+ynakayu@users.noreply.github.com>
2021-09-02 16:15:12 +0900

    09-09 check

--- 09-06_get-data-from-api/js/main.js
+++ 09-06_get-data-from-api/js/main.js
@@ -25,7 +25,7 @@ const app = Vue.createApp({
       axios.get('https://qiita.com/api/v2/items', { params })
            .then(function(response){
              // console.log(response)
-             this.items = response.data
+             vm.items = response.data
            })
            .catch(function(error){
              vm.message = 'Error!' + error
